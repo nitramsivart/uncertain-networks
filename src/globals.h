0852e0da481ef52da553b6d8c5fbecd2f0541868
Travis Martin <nitramsivart@gmail.com>
2015-09-11 16:12:04 -0400

    delete globals

--- src/globals.h
+++
@@ -1,6 +0,0 @@
-// These GLOBAL variables are defined at program initialization, in main.cpp
-extern unsigned long vertices, edges;
-extern unsigned communities;
-extern double edge_ratio, edge_sum, message_converged_diff, zero_thresh;
-extern bool unweighted_degree;
-
